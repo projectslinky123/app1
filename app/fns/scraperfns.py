7082ac6342f911c49e3db1e4cadf255a34407806
bigdesktop <suraj@myprojectfolder.dev>
2021-10-10 18:48:30 -0500

    scrapper fix3

--- app/fns/scraperfns.py
+++ app/fns/scraperfns.py
@@ -33,7 +33,7 @@ def getpagedata(url):
     driver.get(url)
 
     # this is just to ensure that the page is loaded
-    time.sleep(5)
+    time.sleep(3)
 
     html = driver.page_source
     driver.close()  # closing the webdriver
