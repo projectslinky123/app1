7b131252e975165214595df37754b634a3008a25
bigdesktop <suraj@myprojectfolder.dev>
2021-10-10 18:22:23 -0500

    scrapper fix2

--- app/forms.py
+++ app/forms.py
@@ -6,6 +6,7 @@ class InputText(FlaskForm):
     inputtext = TextAreaField('Input Text', validators=[Length(min=0, max=14000)])
     submit = SubmitField('Submit')
 
+
 class InputTicker(FlaskForm):
     ticker = StringField('Enter 1 Stock Ticker', render_kw={"placeholder": "AAPL"}, validators=[Length(min=0, max=5)])
     submit = SubmitField('Submit')
